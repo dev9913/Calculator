5a11eb800bc77405e871e8fdb3bb9324bdf63980
DEV JANGIR <11ganeshji11@gmail.com>
2025-06-08 18:17:58 +0530

    ADD THE FILE FOR CODE DEPLOY

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ WORKDIR /app
 
 COPY index.html .
 
+EXPOSE 5000
+
 CMD ["sh", "-c", "ls -l index.html && echo 'Complete !!'"]
 
---
+++ scripts/strat_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+docker pull docker pull dev7878/index.html
+
+docker run -d -p  5000:5000 docker pull dev7878/index.html
